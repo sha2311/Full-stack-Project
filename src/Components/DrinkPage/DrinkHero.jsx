01d790a227004ad547be17c696c4caa34db6aab0
sha2311 <sha31417@gmail.com>
2025-01-11 13:02:04 +0600

    bg done

--- src/Components/DrinkPage/DrinkHero.jsx
+++ src/Components/DrinkPage/DrinkHero.jsx
@@ -1,9 +1,10 @@
 import { Button } from '@nextui-org/react'
 import React from 'react'
 
+
 const DrinkHero = () => {
   return (
-    <section className='bg-[url("./eat-and-drink-hero-bg.png")] bg-no-repeat bg-top overflow-hidden h-[441px] md:h-[555px] w-full'>
+    <section className='bg-[url("../../../public/eat-and-drink-hero-bg.png")] bg-no-repeat bg-top overflow-hidden h-[441px] md:h-[555px] w-full'>
            <div className='bg-[#FBFCFF] w-[330px] md:w-[558px] h-[140px] md:h-[279px] opacity-85 mx-auto md:mx-[100px] mt-[100px]  rounded-[8px] shadow-md '>
                 <div className='pt-7 mx-2 md:mx-6'>
                     <h2 className='font-bold text-2xl md:text-[42px] md:leading-[37px] text-[#26395C] '>Our top pick for the week!</h2>
